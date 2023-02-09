ef2256f226014ca244bf3b140410fd30d85bba5c
AlecsPassos <116924160+AlecsPassos@users.noreply.github.com>
2023-02-09 20:05:25 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,16 @@ const botaoTrailer = document.querySelector(".botao-trailer");
 const modal = document.querySelector(".modal");
 const botaoFecharModal = document.querySelector(".fechar-modal");
 
+functio alternarmodal(){
+    modal.classList.toggle("aberto");
+}
+
 botaoTrailer.addEventListener("click", () => {
-    modal.classList.add("aberto")
+    alternarModal();
     video.setAttribute("src", linkDoVideo)
 });
 
 botaoFecharModal.addEventListener("click", () => {
-    modal.classList.remove("aberto")
+    alternarModal();
     video.setAttribute("src", "")
     });
